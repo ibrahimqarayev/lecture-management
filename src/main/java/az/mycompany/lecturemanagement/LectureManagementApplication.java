9a35f1ddf66b08636b3164c670bfc7980ab00c0f
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-08-04 23:30:02 +0400

    Initial commit

---
+++ src/main/java/az/mycompany/lecturemanagement/LectureManagementApplication.java
@@ -0,0 +1,13 @@
+package az.mycompany.lecturemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/mycompany/lecturemanagement/LectureManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package az.mycompany.lecturemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
