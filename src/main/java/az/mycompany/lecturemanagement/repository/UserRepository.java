3dffc4b946a1849919eb3262c5c93f1ee835ef99
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-08-04 23:34:03 +0400

    Add UserRepository

---
+++ src/main/java/az/mycompany/lecturemanagement/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package az.mycompany.lecturemanagement.repository;
+
+import az.mycompany.lecturemanagement.entity.User;
+import az.mycompany.lecturemanagement.enums.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    boolean existsByIdentityNumber(String identityNumber);
+
+    List<User> findAllByRole(Role role);
+
+    List<User> findAllByRoleAndIdIsIn(Role role, List<Integer> idList);
+}
