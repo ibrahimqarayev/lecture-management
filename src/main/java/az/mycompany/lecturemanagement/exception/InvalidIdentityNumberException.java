4d10595886390e854e0bdec4c8419fcbb9cef1e5
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-08-05 23:30:38 +0400

    Add InvalidIdentityNumberException

---
+++ src/main/java/az/mycompany/lecturemanagement/exception/InvalidIdentityNumberException.java
@@ -0,0 +1,8 @@
+package az.mycompany.lecturemanagement.exception;
+
+public class InvalidIdentityNumberException extends RuntimeException {
+
+    public InvalidIdentityNumberException(String message) {
+        super(message);
+    }
+}
