798a6e5321ce5968da82d9860a5b9fa33b7de9e6
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-08-06 21:14:10 +0400

    Handle some exceptions

--- src/main/java/az/mycompany/lecturemanagement/exception/GlobalExceptionHandler.java
+++ src/main/java/az/mycompany/lecturemanagement/exception/GlobalExceptionHandler.java
@@ -1,14 +1,59 @@
 package az.mycompany.lecturemanagement.exception;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.List;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex, WebRequest request) {
+        BindingResult bindingResult = ex.getBindingResult();
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+        ErrorResponse errorResponse = new ErrorResponse();
+        for (FieldError fieldError : fieldErrors) {
+            String fieldName = fieldError.getField();
+            String errorMessage = fieldError.getDefaultMessage();
+            String message = "Field name: " + fieldName + " , " + "Error message:" + errorMessage;
+
+            errorResponse.setStatus("error");
+            errorResponse.setHttpStatusCode(HttpStatus.BAD_REQUEST.value());
+            errorResponse.setMessage(message);
+            errorResponse.setPath(((ServletWebRequest) request).getRequest().getRequestURI());
+        }
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException exception, WebRequest request) {
+
+        ErrorResponse errorResponse = new ErrorResponse();
+        for (ConstraintViolation<?> violation : exception.getConstraintViolations()) {
+
+            String fieldName = violation.getPropertyPath().toString();
+            String errorMessage = violation.getMessage();
+            String message = "Field name: " + fieldName + " , " + "Error message:" + errorMessage;
+
+            errorResponse.setStatus("error");
+            errorResponse.setHttpStatusCode(HttpStatus.BAD_REQUEST.value());
+            errorResponse.setMessage(message);
+            errorResponse.setPath(((ServletWebRequest) request).getRequest().getRequestURI());
+        }
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
 
     @ExceptionHandler(InvalidIdentityNumberException.class)
     public ResponseEntity<ErrorResponse> handleInvalidIdentityNumberException(InvalidIdentityNumberException exception, WebRequest request) {
@@ -16,9 +61,8 @@ public class GlobalExceptionHandler {
                 "error",
                 HttpStatus.BAD_REQUEST.value(),
                 exception.getMessage(),
-                request.getDescription(false),
                 ((ServletWebRequest) request).getRequest().getRequestURI());
-        return new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
 
@@ -28,9 +72,8 @@ public class GlobalExceptionHandler {
                 "error",
                 HttpStatus.NOT_FOUND.value(),
                 exception.getMessage(),
-                request.getDescription(false),
                 ((ServletWebRequest) request).getRequest().getRequestURI());
-        return new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(DuplicateResourceException.class)
@@ -39,10 +82,8 @@ public class GlobalExceptionHandler {
                 "error",
                 HttpStatus.CONFLICT.value(),
                 exception.getMessage(),
-                request.getDescription(false),
                 ((ServletWebRequest) request).getRequest().getRequestURI());
-        return new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.CONFLICT);
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
-
 
 }
