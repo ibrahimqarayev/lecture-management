cc29f7d5f15a35c325407aa4b570cf15323b6a5d
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-08-11 22:36:44 +0400

    Update

--- src/main/java/az/mycompany/lecturemanagement/service/LectureService.java
+++ src/main/java/az/mycompany/lecturemanagement/service/LectureService.java
@@ -1,79 +1,71 @@
 package az.mycompany.lecturemanagement.service;
 
+import az.mycompany.lecturemanagement.dto.request.LectureDto;
+import az.mycompany.lecturemanagement.dto.request.UserDto;
+import az.mycompany.lecturemanagement.dto.response.PageInfoWithContent;
 import az.mycompany.lecturemanagement.entity.Lecture;
-import az.mycompany.lecturemanagement.entity.User;
-import az.mycompany.lecturemanagement.exception.DuplicateResourceException;
 import az.mycompany.lecturemanagement.exception.ResourceNotFoundException;
 import az.mycompany.lecturemanagement.repository.LectureRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class LectureService {
     private final LectureRepository lectureRepository;
+    private final UserService userService;
 
-    private final Logger logger = LoggerFactory.getLogger(LectureService.class);
-
-    public LectureService(LectureRepository lectureRepository) {
+    public LectureService(LectureRepository lectureRepository, UserService userService) {
         this.lectureRepository = lectureRepository;
+        this.userService = userService;
     }
 
     @Transactional
-    public Lecture create(Lecture lecture) {
-        if (lecture.getId() == null) {
-            logger.info("Saving lecture");
-            return lectureRepository.save(lecture);
-        }
-        throw new DuplicateResourceException("Lecture already exist");
+    public LectureDto create(Lecture lecture) {
+        return entityToDto(lectureRepository.save(lecture));
     }
 
     @Transactional
     public void deleteById(int id) {
         if (!existsById(id)) {
-            String errorMessage = "Lecture not found with id: " + id;
-            logger.error(errorMessage);
-            throw new ResourceNotFoundException(errorMessage);
+            throw new ResourceNotFoundException("Lecture not found with id: " + id);
         }
-        logger.info("Lecture with id {} is successfully deleted.", id);
         lectureRepository.deleteById(id);
     }
 
-    public List<Lecture> findAll() {
-        List<Lecture> lectures = lectureRepository.findAll();
-        int numberOfLectures = lectures.size();
-        logger.info("Retrieved {} lectures from the database.", numberOfLectures);
-        return lectures;
+    public List<LectureDto> findAll() {
+        return lectureRepository.findAll().stream().map(this::entityToDto).collect(Collectors.toList());
     }
 
-    public Page<Lecture> findAll(Pageable pageable) {
-        Page<Lecture> lecturePage = lectureRepository.findAll(pageable);
-        long numberOfUsers = lecturePage.getTotalElements();
-        logger.info("Retrieved {} lectures with paging from the database.", numberOfUsers);
-        return lecturePage;
+    public PageInfoWithContent<LectureDto> findAll(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("id"));
+        Page<LectureDto> lectureDtoPage = lectureRepository.findAll(pageable).map(this::entityToDto);
+        return new PageInfoWithContent<>(lectureDtoPage);
     }
 
-    public Lecture findById(int id) {
-        Optional<Lecture> optionalLecture = lectureRepository.findById(id);
-        if (optionalLecture.isPresent()) {
-            Lecture lecture = optionalLecture.get();
-            logger.info("Lecture with id {} has been retrieved.", id);
-            return lecture;
-        } else {
-            String errorMessage = "Lecture not found with id: " + id;
-            logger.error(errorMessage);
-            throw new ResourceNotFoundException(errorMessage);
-        }
+    public LectureDto findById(int id) {
+        return entityToDto(lectureRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Lecture not found with id: " + id)));
     }
 
     public boolean existsById(int id) {
         return lectureRepository.existsById(id);
+    }
+
+    public LectureDto entityToDto(Lecture lecture) {
+        LectureDto lectureDto = new LectureDto();
+        lectureDto.setId(lecture.getId());
+        lectureDto.setName(lecture.getName());
+        lectureDto.setStudents(lecture.getStudents().stream().map(userService::entityToDto).collect(Collectors.toList()));
+        lectureDto.setTeacher(userService.entityToDto(lecture.getTeacher()));
+        return lectureDto;
     }
 
 }
